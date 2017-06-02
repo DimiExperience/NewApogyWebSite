59c0ad012243f9ace0d7e2047175b4b9e1040eb7
DimiExperience <stefan.dimi91@gmail.com>
2017-06-02 15:37:13 -0400

    Events page implemented

--- NewApogyWebSite/App_Start/BundleConfig.cs
+++ NewApogyWebSite/App_Start/BundleConfig.cs
@@ -14,6 +14,7 @@ namespace NewApogyWebSite
 
             bundles.Add(new ScriptBundle("~/javascript/bundle").Include(
                 "~/Content/assets/plugins/bootstrap/js/bootstrap.js",
+                "~/Content/assets/js/plugins/owl-carousel.js",
                 "~/Content/assets/plugins/back-to-top.js",
                 "~/Content/assets/plugins/smoothScroll.js",
                 "~/Content/assets/plugins/jquery.parallax.js",
@@ -32,8 +33,6 @@ namespace NewApogyWebSite
                 "~/Content/assets/plugins/bootstrap/css/bootstrap.css",
                 "~/Content/assets/css/app.css",
                 "~/Content/assets/css/style.css",
-                "~/Content/assets/css/headers/header-v6.css",
-                "~/Content/assets/css/footers/footer-v6.css",
                 "~/Content/assets/plugins/animate.css",
                 "~/Content/assets/plugins/line-icons/line-icons.css",
                 "~/Content/assets/plugins/font-awesome/css/font-awesome.css",
@@ -42,6 +41,8 @@ namespace NewApogyWebSite
                 "~/Content/assets/plugins/animated-headline/css/animated-headline.css",
                 "~/Content/assets/css/theme-colors/blue.css",
                 "~/Content/assets/css/theme-skins/dark.css",
+                "~/Content/assets/css/headers/header-v6.css",
+                "~/Content/assets/css/footers/footer-v6.css",
                 "~/Content/assets/css/custom.css"));
 
             bundles.Add(new StyleBundle("~/stylesheets/bundle2").Include(
---
+++ NewApogyWebSite/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewApogyWebSite.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Apogy()
+        {
+            ViewBag.Title = "Apogy";
+
+            return View();
+        }
+        public ActionResult ApogyEvents()
+        {
+            ViewBag.Title = "Apogy Events";
+
+            return View();
+        }
+        public ActionResult Podium()
+        {
+            ViewBag.Title = "Podium";
+
+            return View();
+        }
+    }
+}
