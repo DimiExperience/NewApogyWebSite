30ebf17f237a0b8d408b0a5744ba2be5e8a7bffe
DimiExperience <stefan.dimi91@gmail.com>
2017-06-05 13:04:05 -0400

    Added controllers and views for Solutions and About pages

---
+++ NewApogyWebSite/Controllers/AboutController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewApogyWebSite.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Platform()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Security()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Service()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Support()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Training()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Awards()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Customers()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Team()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+    }
+}
---
+++ NewApogyWebSite/Controllers/SolutionsController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewApogyWebSite.Controllers
+{
+    public class SolutionsController : Controller
+    {
+        public ActionResult CommercialEnablement()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult Onboarding()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult ProductRealignment()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult RegionalTraining()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult SalesKickoffs()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult PlanOfActionMeetings()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult SpeakerPrograms()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult ClassroomTraining()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult CoachingCommunication()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult ContentDelivery()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult FieldLearning()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult SocialAggregation()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult MobileWorkforce()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+        public ActionResult GainingInsights()
+        {
+            ViewBag.Title = "Privacy Policy";
+
+            return View();
+        }
+    }
+}
